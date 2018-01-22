1fecfdcd475cc97856bab4aaf4b817837d12e28c
Kenji <k.saito.1985@gmail.com>
2018-01-22 15:32:30 +0100

    update tubugl-core

---
+++ src/index.js
@@ -0,0 +1,11 @@
+export { Program } from './program';
+export { Program2 } from './program2';
+export { ArrayBuffer } from './arrayBuffer';
+export { IndexArrayBuffer } from './indexArrayBuffer';
+export { Texture } from './texture';
+export { FrameBuffer } from './frameBuffer';
+export { TransformFeedback } from './tranformFeedback';
+export { VAO } from './vao';
+export { draw } from './draw';
+
+export { webGLShader } from './utils/webglShader';
