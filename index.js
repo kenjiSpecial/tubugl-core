c14d86770363f48a7391dd00dc450cd7d0176b13
Kenji Saito <k.saito.1985@gmail.com>
2018-02-03 18:45:32 +0100

    update index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+export { Program } from './src/program';
+export { Program2 } from './src/program2';
+export { ArrayBuffer } from './src/arrayBuffer';
+export { IndexArrayBuffer } from './src/indexArrayBuffer';
+export { Texture } from './src/texture';
+export { FrameBuffer } from './src/frameBuffer';
+export { TransformFeedback } from './src/tranformFeedback';
+export { VAO } from './src/vao';
+export { draw } from './src/draw';
+
+export { webGLShader } from './src/utils/webglShader';
